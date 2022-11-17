c738d5ffbf21a11c330503ecaa1a9e1c38ccdf10
Kunal garg <77530977+Kunalgarg925@users.noreply.github.com>
2022-11-17 22:13:29 +0530

    Add files via upload

---
+++ src/main/java/TakeOrder.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class TakeOrder {
+    Scanner scn = new Scanner(System.in);
+    String specialDays = scn.nextLine();
+    String items = scn.nextLine();
+    String peakHour = scn.nextLine();
+    String nightOrder = scn.nextLine();
+
+}
